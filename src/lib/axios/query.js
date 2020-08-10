10004b66257e541b82bc1c6c992e33e35ff5f4f3
Henry Brown <h@0ti.me>
2020-08-10 12:07:50 -0500

    Simplify default configuration settings

--- src/lib/axios/query.js
+++ src/lib/axios/query.js
@@ -6,18 +6,19 @@ import logAndReturn from '../log-and-return';
 import response2XXHandler from './response-2xx-handler';
 import responseErrorHandler from './response-error-handler';
 
-const instance = axios.create({
-  baseURL: `${get(context, 'config.apiServer.uri')}${get(
-    context,
-    'config.apiServer.basePath',
-  )}`,
-});
+const instance = axios.create(
+  Object.assign(
+    {
+      baseURL: `${get(context, 'config.apiServer.uri')}${get(
+        context,
+        'config.apiServer.basePath',
+      )}`,
+    },
+    get(context, 'config.defaultAxiosArgs', {}),
+  ),
+);
 
+instance.interceptors.request.use(logAndReturn);
 instance.interceptors.response.use(response2XXHandler, responseErrorHandler);
 
-export default (opts = {}) =>
-  instance(
-    logAndReturn(
-      Object.assign({}, get(context, 'config.defaultAxiosArgs', {}), opts),
-    ),
-  );
+export default (opts = {}) => instance(opts);
